e986ea070c32a55e88c74677df96aa4d23c44df1
DawitSishu <dawitalemayhu55@gmail.com>
2023-05-29 09:15:09 -0400

    call-logic

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ const App = () => {
   const [stream,setStream] = useState(null);
   const [userID,setUserID] = useState('');
   const [call,setCall] = useState({});
+  const [userName,setUserName] = useState('');
+  const [inputID,setInputID] = useState('');
 
 
   const friendVid = useRef();
@@ -21,6 +23,7 @@ const App = () => {
     //receive the id 
     socket.on("me",id =>{
       setUserID(id)
+      setUserName(id)
     })
 
 
@@ -43,11 +46,11 @@ const App = () => {
 
 
   //answer (respond to)  the call when end user calls
-  const answerCall = (id) => {
+  const answerCall = () => {
 
     //create a peer connection
     const userPeer = new Peer({
-      initiator: true,
+      initiator: false,
       trickle:false,
       stream,
     })
@@ -63,21 +66,51 @@ const App = () => {
     });
 
     //transmit signal data to the enduser
-    userPeer.signal(call.signal)
+    userPeer.signal(call.signal);
 
     //save the connection instance of the peer
     connection.current = userPeer;
 
   }
 
-  
+  const makeCall = (id) => {
+    //create a peer connection
+    const userPeer = new Peer({
+      initiator: false,
+      trickle:false,
+      stream,
+    });
+
+    //when there is a signal call the provided user with the provided signal data
+    userPeer.on('signal',(data)=>{
+      socket.emit('callUser',{userToCall:id,signal:data,from:userID,name:userName})
+    })
+
+    //when we receive the other person stream saveit locally
+    userPeer.on('stream',(userStream) =>{
+      friendVid.current.srcObject = userStream;
+    });
+
+    //when the call gets accepted set the signal of the call
+    socket.on("callAccepted",(signal)=>{
+      userPeer.signal = signal;
+    })
+
+   //save the connection instance of the peer
+   connection.current = userPeer;
+
+  }
 
   return (
     <div>
+      <h1>user-id: {userID}</h1>
+      <input value={inputID} onChange={e=>setInputID(e.target.value)} />
+      <button onClick={() =>makeCall(inputID)}>call</button>
+      <button onClick={answerCall}>answe</button>
       <h3>current</h3>
-      {/* {console.log(userPeerInstance.current)} */}
-      <video ref={userVid} autoPlay={true}/>
-      <button onClick={callSomeone}>call</button>
+      <video ref={userVid} autoPlay={true} muted/>
+      <h3>remote</h3>
+      <video ref={friendVid} autoPlay={true} />
     </div>
   )
 }
