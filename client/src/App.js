e85394df08d49fb0fd1108b99cacb1d6ea5e9db2
DawitSishu <dawitalemayhu55@gmail.com>
2023-05-29 08:56:25 -0400

    answer-logic

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,16 @@ const socket = io('http://localhost:5000')
 
 const App = () => {
   const [stream,setStream] = useState(null);
-  const [userID,setUserID] = useState();
+  const [userID,setUserID] = useState('');
+  const [call,setCall] = useState({});
 
 
   const friendVid = useRef();
   const userVid = useRef(); 
+  const connection = useRef();
 
-
-  const callSomeone = async() => {
-    const userPeer = new Peer({
-      initiator: true,
-      trickle:false,
-      stream,
-    })
-    console.log(userPeer)
-  }
 
   useEffect(  ()=>{
-
-
     //receive the id 
     socket.on("me",id =>{
       setUserID(id)
@@ -39,10 +30,47 @@ const App = () => {
       setStream(userStream);
     }
 
+    //save the call that is sent
+    socket.on('callUser',({signal,from,name}) =>{
+        setCall({receivingCall:true, signal,callerId : from, callerName : name});
+    })
+
 
     getUserVid();
     
   },[])
+
+
+
+  //answer (respond to)  the call when end user calls
+  const answerCall = (id) => {
+
+    //create a peer connection
+    const userPeer = new Peer({
+      initiator: true,
+      trickle:false,
+      stream,
+    })
+
+    //when a signal is received, respond with peer signal and we have call instance to refer to
+    userPeer.on('signal',(data) =>{
+      socket.emit('answerCall',{to:call.from,signal:data});
+    })
+    
+    //when we recive the remote-user stream save it locally
+    userPeer.on('stream',(userStream) =>{
+      friendVid.current.srcObject = userStream;
+    });
+
+    //transmit signal data to the enduser
+    userPeer.signal(call.signal)
+
+    //save the connection instance of the peer
+    connection.current = userPeer;
+
+  }
+
+  
 
   return (
     <div>
