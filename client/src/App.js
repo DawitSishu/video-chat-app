b3cb48aece7285ef51b5dca0cef9b005420c4c26
DawitSishu <dawitalemayhu55@gmail.com>
2023-05-29 07:59:09 -0400

    server logic-done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,57 @@
-import './App.css';
-import { io  } from "socket.io-client";
+import { useEffect, useRef, useState } from 'react';
+import Peer from 'simple-peer';
+import {io} from 'socket.io-client';
 
-const socket = io.connect("http://localhost:5000")
-function App() {
+
+
+const socket = io('http://localhost:5000')
+
+const App = () => {
+  const [stream,setStream] = useState(null);
+  const [userID,setUserID] = useState();
+
+
+  const friendVid = useRef();
+  const userVid = useRef(); 
+
+
+  const callSomeone = async() => {
+    const userPeer = new Peer({
+      initiator: true,
+      trickle:false,
+      stream,
+    })
+    console.log(userPeer)
+  }
+
+  useEffect(  ()=>{
+
+
+    //receive the id 
+    socket.on("me",id =>{
+      setUserID(id)
+    })
+
+
+    const getUserVid = async() =>{
+      const userStream = await navigator.mediaDevices.getUserMedia({video: true,})
+      userVid.current.srcObject = userStream;
+      setStream(userStream);
+    }
+
+
+    getUserVid();
+    
+  },[])
+
   return (
-    <div className="App">
-      
+    <div>
+      <h3>current</h3>
+      {/* {console.log(userPeerInstance.current)} */}
+      <video ref={userVid} autoPlay={true}/>
+      <button onClick={callSomeone}>call</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const  app = require("express")();
 const server = require("http").createServer(app);
 const dotenv = require("dotenv").config(); 
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000
 
@@ -10,11 +11,30 @@ const io = require("socket.io")(server,{
     }
 });
 
-// app.get('/', (req,res) =>{
-//     res.json("okay")
-// })
+app.use(cors());
+
+app.get('/', (req,res) =>{
+    res.json("okay")
+})
 io.on("connection" ,socket =>{
-    console.log('someone connected...')
+    console.log('someone connected...');
+    
+    //send the socket-id for the user ro use
+    socket.emit("me", socket.id);
+
+
+    // respond to call user event when a peron call and send the necessary data to the other user
+    socket.on("callUser",({userToCall,signal,from,name}) =>{
+        io.to(userToCall).emit("callUser",{signal,from,name});
+    })
+
+    // when someone calls send our signal-data to the person who called
+    socket.on("answerCall",({to,signal}) => {
+        io.to(to).emit("callAccepted",signal);
+    })
+
+
+
 })
 
 
